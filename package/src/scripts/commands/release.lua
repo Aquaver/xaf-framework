b5c6d63e3ca3feae0cc61f4f2865d34c6ab9c6f6
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2018-11-15 08:05:02 +0100

    Added structure for XAF controller 'release' command script source code

---
+++ package/src/scripts/commands/release.lua
@@ -0,0 +1,49 @@
+-- Extensible Application Framework release information program.
+-- This program is able to get current XAF release list and information about specific release.
+-- The user may check latest release before XAF updating.
+
+local arguments, options = ...
+local argument = table.remove(arguments, 1)
+local component = require("component")
+local httpstream = require("xaf/utility/httpstream")
+local jsonparser = require("xaf/utility/jsonparser")
+local xafcore = require("xaf/core/xafcore")
+local xafcoreMath = xafcore:getMathInstance()
+
+local gpu = component.getPrimary("gpu")
+local gpuWidth, gpuHeight = gpu.getResolution()
+
+if (options.h == true or options.help == true) then
+end
+
+if (options.i == nil and options.info == nil and options.l == nil and options.list == nil) then
+end
+
+if (component.isAvailable("internet") == false) then
+  print("---------------------------------------------------")
+  print("-- Extensible Application Framework - Controller --")
+  print("---------------------------------------------------")
+  print("  >> Internet card component is not available")
+  print("  >> Release data retrieving procedure cannot be continued")
+
+  os.exit()
+else
+  print("---------------------------------------------------")
+  print("-- Extensible Application Framework - Controller --")
+  print("---------------------------------------------------")
+  print("  >> Internet card component found")
+  print("  >> Trying to connect to project repository...")
+end
+
+local sourceAddress = "https://api.github.com/repos/Aquaver/xaf-framework/"
+local sourceReleases = "releases"
+local sourceTags = "tags"
+local sourceFlags = "?per_page=10&page=" .. tostring(argument)
+
+local inetAddress = nil
+local inetComponent = component.getPrimary("internet")
+local inetConnection = nil
+
+if (options.i == true or options.info == true) then
+elseif (options.l == true or options.list == true) then
+end

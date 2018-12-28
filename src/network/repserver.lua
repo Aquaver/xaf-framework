82a9aad960400a9b13ef75a351da534c8448309c
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2018-12-28 10:33:58 +0100

    Added function for processing requests in Network:REPServer class source code

--- src/network/repserver.lua
+++ src/network/repserver.lua
@@ -250,6 +250,44 @@ function RepServer:initialize()
 
     return true
   end
+  
+  public.process = function(self, event)                                             -- [!] Function: process(event) - Captures request and passes its object to proper handling function.
+    assert(type(event) == "table", "[XAF Network] Expected TABLE as argument #1")    -- [!] Parameter: event - Event table from OC Event API 'event.pull()' function which holds request object.
+                                                                                     -- [!] Return: status, ... - Processing status as boolean flag and additional request values (unless NO_RETURN choosen).
+    local modem = private.componentModem
+    local port = private.port
+    local address = modem.address
+
+    if (private.active == true) then
+      if (modem) then
+        if (event[1] == "modem_message") then
+          if (event[2] == address and event[4] == port) then
+            local requestName = event[6]
+
+            if (requestName == "REP_EXECUTE") then
+              return true, private:doExecute(event)
+            elseif (requestName == "REP_EXECUTE_ABSOLUTE") then
+              return true, private:doExecuteAbsolute(event)
+            elseif (requestName == "REP_EXECUTE_COMMAND") then
+              return true, private:doExecuteCommand(event)
+            elseif (requestName == "REP_EXECUTE_NO_PROTECT") then
+              return true, private:doExecuteNoProtect(event)
+            elseif (requestName == "REP_EXECUTE_NO_RETURN") then
+              return true, private:doExecuteNoReturn(event)
+            elseif (requestName == "REP_SCRIPT_LIST") then
+              return true, private:doScriptList(event)
+            end
+
+            return false
+          end
+        end
+      else
+        error("[XAF Error] Server network modem component has not been initialized")
+      end
+    else
+      error("[XAF Error] Server is already stopped")
+    end
+  end
 
   return {
     private = private,

647cde8b21dbc3410b7c5ca81d961a911e5d90b2
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2019-02-06 09:16:03 +0100

    Added structure for XAF Package Manager controller 'check' command script source code

---
+++ package/src/scripts/commands/pm/check.lua
@@ -0,0 +1,28 @@
+-- Extensible Application Framework Package Manager add-on update checking program.
+-- This script is used for checking remote version of installed package and comparing their versions.
+-- Every subcommand options is standalone and needs to use flag to run itself.
+
+local arguments, options = ...
+local component = require("component")
+local event = require("event")
+local filesystem = require("filesystem")
+local httpstream = require("xaf/utility/httpstream")
+local jsonparser = require("xaf/utility/jsonparser")
+local xafcore = require("xaf/core/xafcore")
+local xafcoreTable = xafcore:getTableInstance()
+
+local configTable = _G._XAF
+local configVersion = (configTable) and configTable._VERSION or ''
+
+if (options.h == true or options.help == true) then
+end
+
+if (options.p == true or options.package == true) then
+  os.exit()
+end
+
+print("--------------------------------------")
+print("-- XAF Package Manager - Controller --")
+print("--------------------------------------")
+print("  >> Package update checking program")
+print("  >> Use 'xaf-pm check [-h | --help]' for command manual")

3749da6ce5b053efc179c09867cbaa85c38c02f6
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2019-10-21 19:30:14 +0200

    Implemented reverse DNS path in XAF Package Manager controller 'repository' command script source code

--- package/src/scripts/commands/pm/repository.lua
+++ package/src/scripts/commands/pm/repository.lua
@@ -37,7 +37,7 @@ end
 
 if (options.l == true or options.list == true or options.p == true or options.priority == true or -- First group, offline (local only) commands - do not need internet component.
     options.r == true or options.remove == true) then
-      local pathRoot = "aquaver.github.io"
+      local pathRoot = "io.github.aquaver"
       local pathProject = "xaf-framework"
       local pathData = "data"
       local pathName = "pm-source.info"
@@ -289,7 +289,7 @@ if (options.a == true or options.add == true or options.i == true or options.inf
         infoData = ''
 
         if (infoTable["repository-description"] and infoTable["repository-owner"] and infoTable["repository-title"] and infoTable["repository-xaf"]) then
-          local pathRoot = "aquaver.github.io"
+          local pathRoot = "io.github.aquaver"
           local pathProject = "xaf-framework"
           local pathData = "data"
           local pathName = "pm-source.info"

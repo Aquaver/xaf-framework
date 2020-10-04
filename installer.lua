51efa03c50647fc92d31ab30ff8fb14264c6e330
Jakub 'Aquaver' Wyciślik <aquaverpolska@gmail.com>
2020-10-04 19:11:36 +0200

    Updated version number in XAF installation program

--- installer.lua
+++ installer.lua
@@ -18,7 +18,7 @@ local systemVersion = string.sub(systemIdentifier, systemDelimiter)
 
 -- General XAF package installation properties.
 local sourceProject = "https://raw.githubusercontent.com/Aquaver/xaf-framework/"
-local sourceVersion = "1.1.2"
+local sourceVersion = "1.1.3"
 local sourcePackage = "/package"
 local sourcePackageData = "/data"
 local sourceData = {}
